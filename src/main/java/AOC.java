d3bc4f91becf2f8d78c19f2b2fa4249189027e35
kl-erik <kl_erik@outlook.com>
2022-12-21 16:07:01 +0100

    Day 17 part 1 ok

--- src/main/java/AOC.java
+++ src/main/java/AOC.java
@@ -7,6 +7,7 @@ import days.day13.Day13;
 import days.day14.Day14;
 import days.day15.Day15;
 import days.day16.Day16;
+import days.day17.Day17;
 import days.day2.Day2;
 import days.day3.Day3;
 import days.day4.Day4;
@@ -22,10 +23,10 @@ import java.io.FileNotFoundException;
 public class AOC {
     public static void main(String[] args) {
         try {
-            Day[] days = {new Day1(), new Day2(), new Day3(), new Day4(), new Day5(), new Day6(), new Day7(), new Day8(), new Day9(), new Day10(), new Day11(), new Day12(), new Day13(), new Day14(), new Day15(), new Day16()};
-            int day = 16;
+            Day[] days = {new Day1(), new Day2(), new Day3(), new Day4(), new Day5(), new Day6(), new Day7(), new Day8(), new Day9(), new Day10(), new Day11(), new Day12(), new Day13(), new Day14(), new Day15(), new Day16(), new Day17()};
+            int day = 17;
             System.out.println(days[day - 1].puzzle1(new File("src/main/resources/input_" + day + ".txt")));
-            System.out.println(days[day - 1].puzzle2(new File("src/main/resources/input_" + day + ".txt"))); // 2357 too low
+            // System.out.println(days[day - 1].puzzle2(new File("src/main/resources/input_" + day + ".txt")));
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
---
+++ src/test/java/days/day17/Day17Test.java
@@ -0,0 +1,30 @@
+package days.day17;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day17Test {
+    Day17 day = new Day17();
+
+    @Test
+    void puzzle1() {
+        try {
+            assertEquals(3068, day.puzzle1(new File("src/main/resources/example_17.txt")));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void puzzle2() {
+        try {
+            assertEquals(1651, day.puzzle2(new File("src/main/resources/example_17.txt")));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
