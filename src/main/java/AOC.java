1890c740b30d3adfe234d692a1ee2f950aed81ac
kl-erik <kl_erik@outlook.com>
2024-12-06 21:08:17 +0100

    2024-06 p1 ok

--- src/main/java/AOC.java
+++ src/main/java/AOC.java
@@ -5,6 +5,7 @@ import year.year2023.day17.Day17;
 import year.year2024.day03.Day3;
 import year.year2024.day04.Day4;
 import year.year2024.day05.Day5;
+import year.year2024.day06.Day6;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -32,14 +33,15 @@ public class AOC {
                 new Day16(), new Day17()
         };
         aoc[2] = new Day[]{
-                new year.year2024.day01.Day1(), new year.year2024.day02.Day2(), new Day3(), new Day4(), new Day5()
+                new year.year2024.day01.Day1(), new year.year2024.day02.Day2(), new Day3(), new Day4(), new Day5(),
+                new Day6()
         };
 
-        int day = 5;
+        int day = 6;
         int year = 2024;
 
         String path = "src/main/resources/year" + year + "/input_" + (day < 9 ? "0" : "") + day + ".txt";
-        // System.out.println(aoc[year - 2022][day - 1].puzzle1(new File(path)));
-        System.out.println(aoc[year - 2022][day - 1].puzzle2(new File(path)));
+        System.out.println(aoc[year - 2022][day - 1].puzzle1(new File(path)));
+        // System.out.println(aoc[year - 2022][day - 1].puzzle2(new File(path)));
     }
 }
---
+++ src/test/java/year/year2024/day06/Day6Test.java
@@ -0,0 +1,17 @@
+package year.year2024.day06;
+
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Day6Test extends TestCase {
+    Day6 day = new Day6();
+
+    public void testPuzzle1() throws FileNotFoundException {
+        assertEquals(41, day.puzzle1(new File("src/test/resources/year2024/input_06.txt")));
+    }
+
+    public void testPuzzle2() {
+    }
+}
