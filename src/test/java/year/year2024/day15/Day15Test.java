796ad5a4ba16a4104772763a9bc06accca5c2a74
kl-erik <kl_erik@outlook.com>
2024-12-30 13:03:46 +0100

    2024-15 init

---
+++ src/test/java/year/year2024/day15/Day15Test.java
@@ -0,0 +1,18 @@
+package year.year2024.day15;
+
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Day15Test extends TestCase {
+    Day15 day = new Day15();
+
+    public void testPuzzle1() throws FileNotFoundException {
+        assertEquals(null, day.puzzle1(new File("src/test/resources/year2024/input_15.txt")));
+    }
+
+    public void testPuzzle2() throws FileNotFoundException {
+        // assertEquals(null, day.puzzle2(new File("src/test/resources/year2024/input_15.txt")));
+    }
+}
