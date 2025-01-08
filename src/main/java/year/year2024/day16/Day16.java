15389a6afcfb79e0e7638a26e0731f14cf14d26e
kl-erik <kl_erik@outlook.com>
2025-01-08 08:57:10 +0100

    2024-16 p1 & p2 solved with Dijkstra's

--- src/main/java/year/year2024/day16/Day16.java
+++ src/main/java/year/year2024/day16/Day16.java
@@ -6,11 +6,10 @@ import year.Utils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Day16 implements Day {
     enum Direction {
@@ -26,152 +25,122 @@ public class Day16 implements Day {
     @Override
     public Object puzzle1(File file) throws FileNotFoundException {
         char[][] map = Utils.toChars(file);
-        Result minResult = bfs(map).minResult;
-        return minResult.turns * 1000 + minResult.steps;
+        return dijkstra(map).minResult;
     }
 
-    private Solution bfs(char[][] map) {
-        Result[][][] dp = new Result[Direction.values().length][map.length][map[0].length];
-        for (Result[][] dir : dp) {
-            for (Result[] cell : dir) {
-                Arrays.fill(cell, new Result(Integer.MAX_VALUE, Integer.MAX_VALUE));
+    private Solution dijkstra(char[][] map) {
+        Tile[][][] tiles = new Tile[map.length][map[0].length][Direction.values().length];
+
+        Tile startTile = null;
+        Set<Tile> endTiles = new HashSet<>();
+        for (int i = 0; i < tiles.length; i++) {
+            for (int j = 0; j < tiles[i].length; j++) {
+                if (map[i][j] != '#') {
+                    for (Direction dir : Direction.values()) {
+                        Tile tile = new Tile(new Point(j, i), dir, Integer.MAX_VALUE);
+                        tiles[i][j][dir.ordinal()] = tile;
+                        if (map[i][j] == 'S' && dir == Direction.RIGHT) {
+                            tile.dist = 0;
+                            startTile = tile;
+                        } else if (map[i][j] == 'E') {
+                            endTiles.add(tile);
+                        }
+                    }
+                }
+            }
+        }
+        assert startTile != null;
+        assert !endTiles.isEmpty();
+
+        PriorityQueue<Tile> toVisit = new PriorityQueue<>();
+        toVisit.offer(startTile);
+
+        while (!toVisit.isEmpty()) {
+            Tile v = toVisit.poll();
+
+            Set<Tile> neighbours = getNeighbours(v, tiles);
+
+            for (Tile u : neighbours) {
+                int newDist = v.dist + (v.dir == u.dir ? 1 : 1001);
+
+                if (newDist <= u.dist) {
+                    if (newDist < u.dist) {
+                        u.dist = newDist;
+                        toVisit.offer(u);
+                        u.prev.clear();
+                    }
+
+                    u.prev.add(v);
+                }
             }
         }
 
-        Queue<Tile> queue = new LinkedList<>();
-        Point startPoint = getEndPoint(map);
-        for (Direction startDir : Direction.values()) {
-            Point nextPoint = startPoint.add(startDir.v);
-            if (map[nextPoint.y][nextPoint.x] != '#') {
-                Tile tile = new Tile(startPoint, startDir, new Result(0, 0), null);
-                queue.offer(tile);
+        Tile minEndTile = endTiles.stream().min(Tile::compareTo).orElseThrow();
+        Set<Tile> minEndTiles = endTiles.stream().filter(tile -> tile.compareTo(minEndTile) == 0).collect(Collectors.toSet());
+
+        Set<Point> minPaths = new HashSet<>();
+        while (true) {
+            Set<Tile> prevEndTiles = new HashSet<>();
+            for (Tile minTile : minEndTiles) {
+                prevEndTiles.addAll(minTile.prev);
+                minPaths.add(minTile.point);
             }
+            if (prevEndTiles.isEmpty()) {
+                break;
+            }
+            minEndTiles = prevEndTiles;
         }
 
-        Result minResult = new Result(Integer.MAX_VALUE, Integer.MAX_VALUE);
-        Set<Tile> minTiles = new HashSet<>();
+        return new Solution(minEndTile.dist, minPaths.size());
+    }
 
-        while (!queue.isEmpty()) {
-            Tile currentTile = queue.poll();
-            Point point = currentTile.point;
-            Direction dir = currentTile.dir;
-            Result result = currentTile.result;
+    private static Set<Tile> getNeighbours(Tile tile, Tile[][][] tiles) {
+        Direction opposite = Direction.values()[(tile.dir.ordinal() + 2) % Direction.values().length];
 
-            if (map[point.y][point.x] == 'S') {
-                if (dir != Direction.RIGHT) {
-                    result.turns++;
-                }
-
-                if (result.compareTo(minResult) < 0) {
-                    minResult = result;
-                    minTiles.clear();
-                    minTiles.add(currentTile);
-                } else if (result.equals(minResult)) {
-                    minTiles.add(currentTile);
-                }
-
+        Set<Tile> neighbours = new HashSet<>();
+        for (Direction dir : Direction.values()) {
+            if (dir == opposite) {
                 continue;
             }
-
-            if (dp[dir.ordinal()][point.y][point.x].compareTo(result) < 0) {
-                continue;
-            } else {
-                dp[dir.ordinal()][point.y][point.x] = result;
-            }
-
-            for (Direction nextDir : Direction.values()) {
-                Point nextPoint = point.add(nextDir.v);
-                if (map[nextPoint.y][nextPoint.x] != '#') {
-                    Result nextResult = new Result(result.turns + (dir == nextDir ? 0 : 1), result.steps + 1);
-                    queue.offer(new Tile(nextPoint, nextDir, nextResult, currentTile));
-                }
+            Point nextPoint = tile.point.add(dir.v);
+            if (tiles[nextPoint.y][nextPoint.x][dir.ordinal()] != null) {
+                neighbours.add(tiles[nextPoint.y][nextPoint.x][dir.ordinal()]);
             }
         }
-
-        Set<Point> path = new HashSet<>();
-        for (Tile tile : minTiles) {
-            path.add(tile.point);
-            while (tile.prev != null) {
-                path.add(tile.prev.point);
-                tile = tile.prev;
-            }
-        }
-
-        return new Solution(minResult, path);
+        return neighbours;
     }
 
-    private Point getEndPoint(char[][] map) {
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length; j++) {
-                if (map[i][j] == 'E') {
-                    return new Point(j, i);
-                }
-            }
-        }
-
-        throw new RuntimeException("End point not found");
-    }
-
-    private static class Tile {
-        Point point;
+    private static class Tile implements Comparable<Tile> {
+        private final Point point;
         Direction dir;
-        Result result;
-        Tile prev;
+        int dist;
+        Set<Tile> prev = new HashSet<>();
 
-        public Tile(Point point, Direction dir, Result result, Tile prev) {
+        public Tile(Point point, Direction dir, int dist) {
             this.point = point;
             this.dir = dir;
-            this.result = result;
-            this.prev = prev;
-        }
-    }
-
-    static class Result implements Comparable<Result> {
-        int turns;
-        int steps;
-
-        public Result(int turns, int steps) {
-            this.turns = turns;
-            this.steps = steps;
-        }
-
-        public Result add(Result result) {
-            turns += result.turns;
-            steps += result.steps;
-            return this;
+            this.dist = dist;
         }
 
         @Override
-        public int compareTo(Result result) {
-            if (turns == result.turns) {
-                return steps - result.steps;
-            }
-            return turns - result.turns;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof Result)) return false;
-            Result result = (Result) o;
-            return turns == result.turns && steps == result.steps;
+        public int compareTo(Tile tile) {
+            return Integer.compare(dist, tile.dist);
         }
     }
 
     @Override
     public Object puzzle2(File file) throws FileNotFoundException {
         char[][] map = Utils.toChars(file);
-        Set<Point> path = bfs(map).path;
-        return path.size();
+        return dijkstra(map).minPath;
     }
 
     private static class Solution {
-        private final Result minResult;
-        private final Set<Point> path;
+        private final int minResult;
+        private final int minPath;
 
-        public Solution(Result minResult, Set<Point> path) {
+        public Solution(int minResult, int minPath) {
             this.minResult = minResult;
-            this.path = path;
+            this.minPath = minPath;
         }
     }
 }
