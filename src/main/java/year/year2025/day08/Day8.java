443fa21e10b0d6dea35f92eb7ea1dd83eecb6052
kl-erik <kl_erik@outlook.com>
2025-12-09 09:21:42 +0100

    d8 init

---
+++ src/main/java/year/year2025/day08/Day8.java
@@ -0,0 +1,20 @@
+package year.year2025.day08;
+
+import year.Day;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Day8 implements Day {
+    @Override
+    public Object puzzle1(File file) throws FileNotFoundException {
+        // TODO: Implement puzzle1
+        return null;
+    }
+
+    @Override
+    public Object puzzle2(File file) throws FileNotFoundException {
+        // TODO: Implement puzzle2
+        return null;
+    }
+}
---
+++ src/test/java/year/year2025/day08/Day8Test.java
@@ -0,0 +1,18 @@
+package year.year2025.day08;
+
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Day8Test extends TestCase {
+    Day8 day = new Day8();
+
+    public void testPuzzle1() throws FileNotFoundException {
+        // assertEquals(null, day.puzzle1(new File("src/test/resources/year2025/input_08.txt")));
+    }
+
+    public void testPuzzle2() throws FileNotFoundException {
+        // assertEquals(null, day.puzzle2(new File("src/test/resources/year2025/input_08.txt")));
+    }
+}
