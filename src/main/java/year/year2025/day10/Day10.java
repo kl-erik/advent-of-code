137820a588dcd38c326dadecd2afdf1061e1e6db
kl-erik <kl_erik@outlook.com>
2025-12-14 12:51:34 +0100

    d10p1 ok

--- src/main/java/year/year2025/day10/Day10.java
+++ src/main/java/year/year2025/day10/Day10.java
@@ -6,8 +6,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Day10 implements Day {
     @Override
@@ -22,7 +25,47 @@ public class Day10 implements Day {
     }
 
     private int findMinPresses(Machine machine) {
-        return 0;
+        char[] lights = machine.lights;
+        int[][] buttons = machine.buttons;
+
+        for (int n = 0; n < buttons.length; n++) {
+            char[] newLights = new char[lights.length];
+            Arrays.fill(newLights, '.');
+
+            Set<Light> results = pressButtons(newLights, buttons, n, 0);
+
+            for (Light result : results) {
+                if (Arrays.equals(result.lights, lights)) {
+                    return n;
+                }
+            }
+        }
+
+        throw new IllegalArgumentException("No solution found");
+    }
+
+    private Set<Light> pressButtons(char[] lights, int[][] buttons, int n, int i) {
+        if (n == 0) {
+            return Collections.singleton(new Light((lights)));
+        }
+
+        Set<Light> results = new HashSet<>(Collections.emptySet());
+
+        char[] newLights = Arrays.copyOf(lights, lights.length);
+        for (int j = i; j < buttons.length; j++) {
+            char[] pressedLights = pressButton(newLights, buttons[j]);
+            results.addAll(pressButtons(pressedLights, buttons, n - 1, j + 1));
+        }
+
+        return results;
+    }
+
+    private char[] pressButton(char[] lights, int[] button) {
+        char[] newLights = Arrays.copyOf(lights, lights.length);
+        for (int index : button) {
+            newLights[index] = newLights[index] == '.' ? '#' : '.';
+        }
+        return newLights;
     }
 
     @Override
@@ -46,13 +89,21 @@ public class Day10 implements Day {
         return machines;
     }
 
-    private class Machine {
+    private static class Machine {
         private final char[] lights;
         private final int[][] buttons;
 
         public Machine(char[] lights, int[][] buttons) {
             this.lights = lights;
             this.buttons = buttons;
+        }
+    }
+
+    private static class Light {
+        private final char[] lights;
+
+        public Light(char[] lights) {
+            this.lights = lights;
         }
     }
 }
